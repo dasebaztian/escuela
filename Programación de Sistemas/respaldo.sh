75fdb9da240003df24430b0052a6f40f71075ef2
Sebastian Sanchez <danielsebastiansanchezm@gmail.com>
2023-09-10 09:49:53 -0600

    Terminando primer script

--- Programación de Sistemas/respaldo.sh
+++ Programación de Sistemas/respaldo.sh
@@ -8,7 +8,6 @@ solo_nombre="$(basename $archivo)"
 fecha=$(date +%Y-%m-%d)
 ruta_destino_completa=$ruta_destino$solo_nombre$fecha
 
-echo "$USER" > $ruta_destino_completa
+echo "$USER" > /tmp/user.txt
+cat /tmp/user.txt $archivo > $ruta_destino_completa
 
-cat "$ruta_destino_completa tmp" $archivo >> $ruta_destino_completa
-echo $ruta_destino_completa
